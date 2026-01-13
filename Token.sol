3aba30a7f4035bd4dc71a959c6eb06b4174e88b5
Yashwanth S K <151870496+Yashwanth-S-K@users.noreply.github.com>
2026-01-13 11:13:45 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Conduit_Agent_Token is ERC20{
+    constructor() ERC20("Conduit_Agent_Token", "CAT"){
+         _mint(msg.sender,10000*10**18);
+    }
+}
